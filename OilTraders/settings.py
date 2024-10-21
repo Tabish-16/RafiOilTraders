734ca473fa29f8140d5294a68cb7aedf157b9a46
Tabish-16 <79455085+Tabish-16@users.noreply.github.com>
2024-10-21 16:25:39 +0500

    Update settings.py

--- OilTraders/settings.py
+++ OilTraders/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jtjgm69y@ow!s0&puxt@7%b639jm6$zu8)inx+rot2n@lcj%jm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["www.rafioiltraders.onrender.com"]
 
